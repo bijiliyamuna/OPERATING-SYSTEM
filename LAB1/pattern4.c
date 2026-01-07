c0fe5fc610137f546a7e68d17abc811db569322b
bijiliyamuna <bijiliyamuna@gmail.com>
2026-01-07 12:19:08 +0530

    Add files via upload

---
+++ LAB1/pattern4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+  int n,i,j,k;
+  printf("enter the n value:");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++){
+  	for(k=1;k<=n-i;k++){
+  	printf(" ");
+  	}
+  	for(j=1;j<=i;j++){
+  		printf("%d",j);
+  	}
+  	for(j=i-1;j>=1;j--){
+  		printf("%d",j);
+  	}
+  	printf("\n");
+  	
+  }
+}
+
